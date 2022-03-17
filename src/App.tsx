5a88224a4908551441857b8bdc6a3d5966dacceb
Luan Tonin Galvan <ltoningalvan@gmail.com>
2022-03-16 21:18:23 -0300

    feat: adiciona estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { globalCss } from "@stitches/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const ShowCasePage = () => <p>Showcase</p>;
 const HeroPage = () => <p>Hero</p>;
 
+const globalStyles = globalCss({
+  "*": { margin: 0, padding: 0 },
+
+  body: {
+    fontFamily:
+      '-apple-system, Ubuntu, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+  },
+});
+
 function App() {
+  globalStyles();
+
   return (
     <BrowserRouter>
       <Routes>
